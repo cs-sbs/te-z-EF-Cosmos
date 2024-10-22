e20c3b2cd443c6654fd3b11e1ee5911c64e3c325
EF-Cosmos <93043071+EF-Cosmos@users.noreply.github.com>
2024-10-22 19:49:50 +0800

    Add files via upload

--- src/main/java/org/example/OddEven.java
+++ src/main/java/org/example/OddEven.java
@@ -2,24 +2,38 @@ package org.example;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
 public class OddEven {
+
+    public static int isOddorEven(int num){
+        int odd =0;
+        int even =0;
+        if ((num & 1) == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
-        int number;
+        Scanner scanner = new Scanner(System.in);
         int countOdd = 0;
         int countEven = 0;
-        Scanner in = new Scanner(System.in); // 数字用空格或换行隔开，每次读入一个，直到输入‘-1’
-        do{
-            number = in.nextInt();
-            if(number != -1)
-            {
-                if (number % 2 == 0) {
-                    countEven += 1;
-                } else {
-                    countOdd += 1;
-                }
+
+        while (true) {
+            int num = scanner.nextInt();  // 读取输入
+            if (num == -1) {              // 如果是-1则结束输入
+                break;
             }
-        }while(number != -1); // 理解do while和while循环的区别
-        System.out.print(countOdd+" ");
-        System.out.println(countEven);
+
+            // 使用位运算符判断奇偶
+            if ((num & 1) == 0) {
+                countEven++;  // 偶数
+            } else {
+                countOdd++;   // 奇数
+            }
+        }
+        System.out.println(countOdd + " " + countEven);
     }
 }
+
